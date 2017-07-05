e91dc55e780c6d95f338c57d8fe91cd7432931db
Jared Luedtke and John Dowd <jared+john@>
2017-07-05 15:49:02 -0700

    finish wednesday lessons

---
+++ app/task-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component ({
+  selector: 'task-list',
+  template: `
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
+      <ul>
+        <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompletness">{{currentTask.description}} {{currentTask.priority}}
+          <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+          <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+          <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+        </li>
+      </ul>
+        `
+})
+
+export class TaskListComponent {
+  filterByCompletness: string = "incompleteTasks";
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+}
+
+  onChange(optionFromMenu) {
+    this.filterByCompletness = optionFromMenu;
+  }
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+   clickedTask.done = setCompleteness;
+ }
+
+}
